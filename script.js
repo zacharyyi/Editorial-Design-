4adc9114ab00d423f2d49162ed84b46fa21955d4
zacharyyi <145707584+zacharyyi@users.noreply.github.com>
2023-11-28 19:31:43 +0000

    beta version

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 console.log('😀');
+
+// defaults
+ScrollTrigger.defaults({
+    toggleActions: "restart pause resume pause"
+  });
+  
+  // Panel One
+  gsap.to(".one h1", {
+    scrollTrigger: "one",
+    color: "brown",
+    duration: 3,
+    rotate: 0,
+    opacity: 1
+  });
+  
+  // Panel Two
+  gsap.to(".two", {
+    scrollTrigger: "two"
+  });
+  
+  // Panel Three
+  gsap.to(".three p", {
+    scrollTrigger: "three",
+    letterSpacing: 0,
+    duration: 5
+  });
+  
+  // Panel Four
+  gsap.to(".four", {
+    scrollTrigger: "four"
+  });
+  
+  // Panel Five
+  gsap.to(".five", {
+    scrollTrigger: "five"
+  });
+  
+  // Panel Six
+  gsap.to(".six", {
+    scrollTrigger: "six",
+    translateX: 0,
+    duration: 5
+  });
