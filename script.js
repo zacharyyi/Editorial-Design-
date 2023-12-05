248694d520c875cb23d555092ebf973fa4a75a38
zacharyyi <145707584+zacharyyi@users.noreply.github.com>
2023-12-05 21:34:15 +0000

    updated Beta version

--- script.js
+++ script.js
@@ -1,39 +1,33 @@
 console.log('😀');
 
-// defaults
-ScrollTrigger.defaults({
-    toggleActions: "restart pause resume pause"
-  });
-  
-  // Panel One
-  gsap.to(".one h1", {
-    scrollTrigger: "one",
-    color: "brown",
-    duration: 3,
-    rotate: 0,
-    opacity: 1
-  });
-  
-  // Panel Two
-  gsap.to(".two", {
-    scrollTrigger: "two"
-  });
-  
-  // Panel Three
-  gsap.to(".three p", {
-    scrollTrigger: "three",
-    letterSpacing: 0,
-    duration: 5
-  });
-  
-  // Panel Four
-  gsap.to(".four", {
-    scrollTrigger: "four"
-  });
-  
-  // Panel Five
-  gsap.to(".five", {
-    scrollTrigger: "five"
-  });
-  
-  // Panel Six
+// defaultsgsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
+
+let getRatio = (el) =>
+  window.innerHeight / (window.innerHeight + el.offsetHeight);
+
+gsap.utils.toArray("section").forEach((section, i) => {
+  section.bg = section.querySelector(".bg");
+
+  // the first image (i === 0) should be handled differently because it should start at the very top.
+  // use function-based values in order to keep things responsive
+  gsap.fromTo(
+    section.bg,
+    {
+      backgroundPosition: () =>
+        i ? `50% ${-window.innerHeight * getRatio(section)}px` : "50% 0px"
+    },
+    {
+      backgroundPosition: () =>
+        `50% ${window.innerHeight * (1 - getRatio(section))}px`,
+      ease: "none",
+      scrollTrigger: {
+        trigger: section,
+        start: () => (i ? "top bottom" : "top top"),
+        end: "bottom top",
+        scrub: true,
+        invalidateOnRefresh: true // to make it responsive
+      }
+    }
+  );
+});
